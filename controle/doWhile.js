6688393b25ef0c9213fd556a013cf6c9111db33f
valtercavalcante06 <58271713+valtercavalcante06@users.noreply.github.com>
2020-01-17 18:05:16 -0300

    dowhile

--- controle/doWhile.js
+++ controle/doWhile.js
@@ -2,10 +2,10 @@ const retornaInteiroAleatorio = (min,max) => {
     const numero = Math.random() * (max - min) + min
     return Math.floor(numero)
 }
+console.log("teste")
+let opcao = -1
 
-let opcao = 0
-
-while (opcao != -1){
+do {
     opcao = retornaInteiroAleatorio(-1,10)
     console.log("A opcao escolhida doi " + opcao)
-}
+}while (opcao != -1)
---
+++ controle/for1.js
@@ -0,0 +1,5 @@
+let contator = 1
+while(contador <= 10){
+    console.log('contador = ${contador}')
+    contador ++
+}
